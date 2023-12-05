d9d23603e44c9a8056aae646d76486d68486f020
evaBro100 <kharchenko.eva@gmail.com>
2023-12-05 09:40:06 +0500

    lesson 8 done

--- index.js
+++ index.js
@@ -59,8 +59,70 @@ function spinWords(let){
 }
 
 //4
-function indexSum(nums, target){
 
+const nums = [2,7,11,15];
+let target = 9;
+const result3 = indexSum(nums, target);
+console.log('Задание 4\n', result3)
+
+function indexSum(arr, sum){
+    for (let i = 0; i <  arr.length; i++){
+        for (let j = i + 1; j < arr.length; j++){
+            if (arr[i]+arr[j]==sum){
+                return [i,j]
+            }
+        }
+    }
 }
+
+//5
+// const strs = ["цветок","поток","хлопок"]
+// const result4 = findPrefix(strs);
+// console.log(result4);
+
+// function findPrefix(str){
+//     let prefix = str[0]; 
+//     for (let i = 1; i <strs.length; i++){
+//         while (strs[i].indexOf(prefix)!= -1){
+//             let prefix = prefix.substring(0, prefix.length-1);
+//             if (prefix ==''){
+//                 return '';
+//             }
+//         }
+//     }
+//     return prefix;
+// }
+
+function findLongestCommonPrefix(strs) {
+    if (!strs || strs.length === 0) {
+        return "";
+    }
+
+    let longestCommonPrefix = "";
+    let minLength = Math.min([...strs].map(str => str.length));
+
+    for (let i = 0; i < minLength; i++) {
+        const char = strs[0][i];
+
+        if (strs.every(str => str[i] === char)) {
+            longestCommonPrefix += char;
+        } else {
+            break;
+        }
+    }
+
+    if (longestCommonPrefix.length < 2) {
+        return "";
+    }
+
+    return longestCommonPrefix;
+}
+
+const example1 = findLongestCommonPrefix(["цветок", "поток", "хлопок"]);
+console.log(example1);
+
+const example2 = findLongestCommonPrefix(["собака", "гоночная машина", "машина"]);
+console.log(example2);
+
 
 
